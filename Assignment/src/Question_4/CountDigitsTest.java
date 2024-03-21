e5a552355321d8050779770d9bf97c68dc73e62c
SENFORD <senfordfernandes201@gmail.com>
2024-03-21 18:47:23 +0530

    Question 4

---
+++ Assignment/src/Question_4/CountDigitsTest.java
@@ -0,0 +1,17 @@
+package Question_4;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CountDigitsTest {
+
+    @Test
+    public void testCountDigitsPositive() {
+        assertEquals(4, CountDigits.countDigits(1234));
+    }
+
+    @Test
+    public void testCountDigitsZero() {
+        assertEquals(1, CountDigits.countDigits(0));
+    }
+}
